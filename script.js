dd9ef95eeae43d51006afa88b31cfdf5f688ab1e
hwen80 <simonldx@gmail.com>
2019-06-26 16:11:07 +0200

    rewrote script.js using async functions

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
 var list;
-fetch('articles/head.md')
-	.then(content => content.text())
-	.then(text => document.body.insertAdjacentHTML('beforeend', text));
-if (tag) {
-    fetch('articles/articles.json')
-        .then(content => content.json())
-        .then(json => list = json)
-        .then(() =>
-            list.articles = list.articles.filter(article => article.tags.includes("all") || article.tags.includes(tag)))
-        .then(() =>
-            fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text)))
-        .then(() =>
-            fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text)));
-}
-else if (title) {
-    fetch('articles/articles.json')
-        .then(content => content.json())
-        .then(json => list = json)
-        .then(() =>
-            list.articles = list.articles.filter(article => article.title == title))
-        .then(() =>
-            fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text)))
-}
-else {
-    fetch('articles/articles.json')
-        .then(content => content.json())
-        .then(json => list = json)
-        .then(() =>
-            list.articles = list.articles.filter(article => !article.tags.includes('none')))
-        .then(() => 
-            fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text)))
-        .then(() =>
-            fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text)));
+
+const appendArticle = async (filepath) => {
+	const content = await fetch(filepath);
+	const text = await content.text();
+
+	document.body.insertAdjacentHTML('beforeend', text);
 }
 
-window.onscroll = function() {
-    if (window.innerHeight + document.body.scrollTop + 1 >= document.documentElement.offsetHeight && list.articles.length > 0) {
-        fetch('articles/' + list.articles.shift().filepath)
-            .then(content => content.text())
-            .then(text => document.body.insertAdjacentHTML('beforeend', text));
-    }
+const filterArticle = async (filter) => {
+	const content = await fetch('articles/articles.json');
+	const json = await content.json();
+
+	return json.articles.filter(article => filter(article));
 }
+
+appendArticle('articles/head.md');
+
+(async function(){
+	if (tag) {
+		list = await filterArticle(article => article.tags.includes("all") || article.tags.includes(tag));
+	}
+	else if (title) {
+		list = await filterArticle(article => article.title == title);
+	}
+	else {
+		list = await filterArticle(article => !article.tags.includes('none'));
+	}
+
+	while (list.length > 0 && (window.innerHeight + document.body.scrollTop + 1 >= document.documentElement.offsetHeight || list[0].multipart)) {
+		await appendArticle('articles/' + list.shift().filepath);
+	}
+
+	window.onscroll = async function() {
+		if (window.innerHeight + document.body.scrollTop + 1 >= document.documentElement.offsetHeight && list.length > 0) {
+			appendArticle('articles/' + list.shift().filepath);
+		}
+	}
+})();
